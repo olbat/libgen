4bcb4f23ab0635ccc9c83384a8f0aeeb5c7e8959
Luc Sarzyniec <devel@olbat.net>
2017-04-06 17:55:25 +0200

    Add specs for the NodeCounter class

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,11 @@
 require "spec"
 require "../src/lib_generator"
+
+# FIXME: ugly hack, find something else
+def ast_node(source : String) : Crystal::ASTNode
+  Crystal::Parser.parse("lib L\n#{source}\nend").as(Crystal::LibDef).body
+end
+
+def ast_nodes(sources : Array(String)) : Array(Crystal::ASTNode)
+  Crystal::Parser.parse("lib L\n#{sources.join("\n")}\nend").as(Crystal::LibDef).body.as(Crystal::Expressions).expressions
+end
