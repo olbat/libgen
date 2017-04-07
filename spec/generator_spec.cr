488019b3dd0fc58304593171643ac21cb29f45f7
Luc Sarzyniec <devel@olbat.net>
2017-04-07 18:56:23 +0200

    Add specs for the Generator class

---
+++ spec/generator_spec.cr
@@ -0,0 +1,94 @@
+require "./spec_helper"
+
+describe "LibGenerator::Generator::Lib" do
+  # TODO: to be completed
+end
+
+describe "LibGenerator::Generator" do
+  describe "initialize" do
+    it "creates a new generator" do
+      library = LibGenerator::Library.new("LibFoo", "-lfoo", includes: ["bar.yml"])
+      definitions = {"foo" => LibGenerator::Definition.new}
+      transformers = [LibGenerator::SortTransformer.new.as(Crystal::Transformer)]
+      generator = LibGenerator::Generator.new(library, definitions, transformers)
+      generator.libs.keys.should eq(["foo"])
+    end
+  end
+
+  # TODO: to be simplified + create a helper
+  describe "extract_common_nodes" do
+    it "extracts common nodes" do
+      sources = [
+        "fun foo\nfun bar",
+        "fun foo\nfun foobar",
+        "fun bar",
+        "fun barfoo",
+      ]
+
+      library = LibGenerator::Library.new("LibFoo", "-lfoo", includes: ["bar.yml"])
+      definitions = sources.size.times.map(&.to_s).to_a.zip(sources.map { LibGenerator::Definition.new }).to_h
+      generator = LibGenerator::Generator.new(library, definitions, [] of Crystal::Transformer)
+
+      generator.libs.each_with_index do |(_, li), i|
+        li.ast = Crystal::Parser.parse("lib L\n#{sources[i]}\nend")
+      end
+
+      generator.extract_common_nodes.should eq(ast_nodes(["fun foo", "fun bar"]))
+    end
+  end
+
+  describe "group_common_nodes" do
+    it "groups common nodes in a lib and generates requires" do
+      sources = [
+        "fun foo\nfun bar",
+        "fun foo\nfun foobar",
+        "fun bar",
+        "fun barfoo",
+      ]
+
+      library = LibGenerator::Library.new("LibFoo", "-lfoo", includes: ["bar.yml"])
+      definitions = sources.size.times.map(&.to_s).to_a.zip(sources.map { LibGenerator::Definition.new }).to_h
+      generator = LibGenerator::Generator.new(library, definitions, [] of Crystal::Transformer)
+
+      generator.libs.each_with_index do |(_, li), i|
+        li.ast = Crystal::Parser.parse("lib L\n#{sources[i]}\nend")
+      end
+
+      generator.group_common_nodes
+
+      li = generator.libs[generator.common_filename]
+
+      li.ast.should eq(Crystal::Expressions.new(ast_nodes(["fun foo", "fun bar"])))
+      li.requires.should eq(definitions.keys.map { |fn| "./#{fn}" })
+    end
+
+    it "groups common nodes in an existing lib and generates requires" do
+      sources = [
+        "fun foo\nfun bar",
+        "fun foo\nfun foobar",
+        "fun bar",
+        "fun barfoo",
+      ]
+
+      library = LibGenerator::Library.new("LibFoo", "-lfoo", includes: ["bar.yml"])
+      definitions = sources.size.times.map(&.to_s).to_a.zip(sources.map { LibGenerator::Definition.new }).to_h
+      common_filename = "lib_foo.cr"
+      definitions[common_filename] = LibGenerator::Definition.new
+
+      generator = LibGenerator::Generator.new(library, definitions, [] of Crystal::Transformer)
+
+      generator.libs.each_with_index do |(_, li), i|
+        li.ast = Crystal::Parser.parse("lib L\n#{sources[i]}\nend") if i < sources.size
+      end
+
+      generator.libs[common_filename].ast = Crystal::Expressions.new([ast_node("fun common")])
+
+      generator.group_common_nodes
+
+      li = generator.libs[generator.common_filename]
+
+      li.ast.should eq(Crystal::Expressions.new(ast_nodes(["fun common", "fun foo", "fun bar"])))
+      li.requires.should eq(sources.size.times.to_a.map { |i| "./#{i}" })
+    end
+  end
+end
