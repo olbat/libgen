fdc65101e57154a35ddc63133ac37465bbac93bc
Luc Sarzyniec <devel@olbat.net>
2017-04-14 08:55:32 +0200

    Fix: do not generate files with empty libs
    Files containing empty libs can be generated if every definitions
    of a header moved to the common file. The Generator#generate method
    has been updated and can now generate nil sources for a given file.

--- examples/openssl/src/lib_openssl/lib_openssl.cr
+++ examples/openssl/src/lib_openssl/lib_openssl.cr
@@ -1,7 +1,3 @@
-@[Link(ldflags: "`command -v pkg-config > /dev/null && pkg-config --libs openssl || printf %s '-lssl -lcrypto'`")]
-lib LibOpenSSL
-end
-
 require "./sha.cr"
 require "./md5.cr"
 require "./rand.cr"
--- spec/remove_transformer_spec.cr
+++ spec/remove_transformer_spec.cr
@@ -8,12 +8,14 @@ describe "LibGenerator::RemoveTransformer" do
         "fun b",
         "fun c",
         "fun d",
+        "fun e",
       ]
       ast = Crystal::Parser.parse("lib L\n#{sources.join("\n")}\nend")
 
       transformer = LibGenerator::RemoveTransformer.new(ast_nodes(sources[0..1]))
 
       ast.transform(transformer)
+      ast.as?(Crystal::LibDef).not_nil!.body.as(Crystal::Expressions).expressions.size.should eq(3)
       ast.to_s.should eq("lib L\n  #{sources[2..-1].join("\n  ")}\nend")
     end
   end
