1b9df364eeb8890bd77b18d6e1780ac5a7864228
Luc Sarzyniec <devel@olbat.net>
2017-04-26 17:52:31 +0200

    Fix: transformers does not transform children nodes

--- src/lib_generator/transformers/sort.cr
+++ src/lib_generator/transformers/sort.cr
@@ -3,6 +3,6 @@ require "compiler/crystal/syntax"
 class LibGenerator::SortTransformer < Crystal::Transformer
   def transform(node : Crystal::Expressions)
     node.expressions.sort_by!(&.to_s)
-    node
+    super
   end
 end
