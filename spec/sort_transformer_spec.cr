d6f763036def7c00b242a3304dca082a4e754cb7
Luc Sarzyniec <devel@olbat.net>
2017-04-26 18:53:36 +0200

    Fix: the sort transformer sorts struct and union fields

--- spec/sort_transformer_spec.cr
+++ spec/sort_transformer_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 
 describe "LibGenerator::SortTransformer" do
   describe "transform" do
-    it "sorts by node type and name" do
+    it "sorts lib's nodes by type and name" do
       sources = [
         "fun c",
         "fun a",
@@ -15,6 +15,29 @@ describe "LibGenerator::SortTransformer" do
 
       ast.transform(transformer)
       ast.to_s.should eq("lib L\n  #{sources.sort.join("\n  ")}\nend")
+    end
+
+    it "doesn't sort struct's and union's fields" do
+      sources = <<-EOS
+        lib L
+          struct Foo
+            a : Int32
+            c : Int8
+            b : Int16
+          end
+          union Bar
+            x : Int32
+            z : Int8
+            y : Int16
+          end
+        end
+        EOS
+      ast = Crystal::Parser.parse(sources)
+
+      transformer = LibGenerator::SortTransformer.new
+
+      ast.transform(transformer)
+      ast.to_s.should eq(sources)
     end
   end
 end
