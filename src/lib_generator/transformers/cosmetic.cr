0a89a697ccd7ef43115c8e12e87cb532a8b90dc9
Luc Sarzyniec <devel@olbat.net>
2017-05-09 08:26:29 +0200

    Add an englobing class for transformers that performs cosmetic modifications

---
+++ src/lib_generator/transformers/cosmetic.cr
@@ -0,0 +1,4 @@
+# englobing class for transformers that does not perform semantic modifications
+# (re-ordering nodes, formating, etc ...)
+class LibGenerator::CosmeticTransformer < Crystal::Transformer
+end
