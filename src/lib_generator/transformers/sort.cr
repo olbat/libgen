c0c845d7fc56e49f2752ad528e4022d76b5a9555
Luc Sarzyniec <devel@olbat.net>
2017-05-09 08:26:29 +0200

    Add an englobing class for transformers that performs cosmetic modifications

--- src/lib_generator/transformers/sort.cr
+++ src/lib_generator/transformers/sort.cr
@@ -1,6 +1,6 @@
 require "compiler/crystal/syntax"
 
-class LibGenerator::SortTransformer < Crystal::Transformer
+class LibGenerator::SortTransformer < LibGenerator::CosmeticTransformer
   def transform(node : Crystal::Expressions)
     node.expressions.sort_by!(&.to_s)
     super
