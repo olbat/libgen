9f1b28bb5d4205c5e04ff007f9fecccffbeb7743
Luc Sarzyniec <devel@olbat.net>
2021-03-01 20:59:32 +0100

    Fix: error in LibICU integration test due to uninitialized status variable

--- spec/integration/lib_icu.cr
+++ spec/integration/lib_icu.cr
@@ -5,13 +5,15 @@ describe "LibICU" do
   describe "ucsdet" do
     it "detects encoding" do
       text = File.read(File.join(__DIR__, "..", "..", "LICENSE"))
-      ustatus = uninitialized LibICU::UErrorCode
+
+      ustatus = LibICU::UErrorCode::UZeroError
 
       csdet = LibICU.ucsdet_open(pointerof(ustatus))
       begin
         LibICU.ucsdet_set_text(csdet, text, text.size, pointerof(ustatus))
         csmatch = LibICU.ucsdet_detect(csdet, pointerof(ustatus))
 
+        ustatus.should eq(LibICU::UErrorCode::UZeroError)
         String.new(LibICU.ucsdet_get_name(csmatch, pointerof(ustatus))).should eq("ISO-8859-1")
         String.new(LibICU.ucsdet_get_language(csmatch, pointerof(ustatus))).should eq("en")
         LibICU.ucsdet_get_confidence(csmatch, pointerof(ustatus)).should eq(76)
